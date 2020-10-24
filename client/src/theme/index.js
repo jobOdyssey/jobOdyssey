1dc7e71f1fca03dfb9c3b84858d60d7e6afb4093
olanrewaju Makinde <lanre.developer@gmail.com>
2020-10-24 19:15:47 +0100

    added login, signup & home styles to theme

--- client/src/theme/index.js
+++ client/src/theme/index.js
@@ -1,4 +1,5 @@
 import {DefaultTheme} from 'react-native-paper';
+import {StyleSheet} from 'react-native';
 
 export const AppTheme = {
   ...DefaultTheme,
@@ -9,3 +10,52 @@ export const AppTheme = {
     accent: '#e1ffb1',
   },
 };
+
+export const HomeStyles = StyleSheet.create({
+  board: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export const LoginStyles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  buttons: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+  },
+  board: {
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    padding: 10,
+  },
+  input: {
+    margin: 10,
+  },
+});
+
+export const SignupStyles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  buttons: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+  },
+  board: {
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    padding: 10,
+  },
+  input: {
+    margin: 10,
+  },
+});
