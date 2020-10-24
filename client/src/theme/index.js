bacfb352be7460f0f18461fc973e58d37438e799
olanrewaju Makinde <lanre.developer@gmail.com>
2020-10-24 17:59:15 +0100

    created a theme module

---
+++ client/src/theme/index.js
@@ -0,0 +1,11 @@
+import {DefaultTheme} from 'react-native-paper';
+
+export const AppTheme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#aed581',
+    accent: '#e1ffb1',
+  },
+};
